2d321d964d0aeb932e2486621e41d8521d2cc949
Guzio <guzekk@protonmail.com>
2026-09-22 12:08:37 +0200

    fix run_proxy_tcp: command not found
    I first assumed that you can't call functions via $command, but then I noticed a bunch of typos. But the previous "fix" was already applied. Oh, well!

--- ipv6-proxy.sh
+++ ipv6-proxy.sh
@@ -12,25 +12,20 @@ start_processes() {
 		exit 1;
 	fi
 
-    command1="run_proxy_tcp \"$TARGET\" 80"
-    command2="run_proxy_tcp \"$TARGET\" 443"
-    command3="run_proxy_tcp \"$TARGET\" 25565"
-    command4="run_proxy_udp \"$TARGET\" 8024454"
-    
     # Run commands in the background
-    $command1 &
+    run_proxy_tcp "$TARGET" 80 &
     pid1=$!
-    $command2 &
+    run_proxy_tcp "$TARGET" 443 &
     pid2=$!
-    $command3 &
+    run_proxy_tcp "$TARGET" 25565 &
     pid3=$!
-    $command4 &
+    run_proxy_udp "$TARGET" 24454 &
     pid4=$!
 
     echo "Processes started with PIDs: $pid1, $pid2, $pid3 and $pid4, and targeting $TARGET. Use kill -9 $pid1 $pid2 $pid3 $pid4 to stop."
 }
 
-run_proxy_tpc() {
+run_proxy_tcp() {
 	while true
 	do
 		sudo socat TCP4-LISTEN:$2,fork,su=nobody,reuseaddr "TCP6:[$1]:$2"
@@ -39,7 +34,7 @@ run_proxy_tpc() {
 	done
 }
 
-run_proxy_upd() {
+run_proxy_udp() {
 	while true
 	do
 		sudo socat UDP4-LISTEN:$2,fork,su=nobody,reuseaddr "UDP6:[$1]:$2"
