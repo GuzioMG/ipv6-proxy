cbf0d4451bb2d4915d51bb403a72d1198d3538a7
Guzio <guzekk@protonmail.com>
2026-09-22 11:55:51 +0200

    added auto-restart capabilities

--- ipv6-proxy.sh
+++ ipv6-proxy.sh
@@ -5,10 +5,17 @@ set -euo pipefail
 start_processes() {
     echo "Starting proxy..."
     TARGET=$(dig +short AAAA "$(cat target.txt)" | head -n1)
-    command1="sudo socat TCP4-LISTEN:80,fork,su=nobody,reuseaddr TCP6:[$TARGET]:80"
-    command2="sudo socat TCP4-LISTEN:443,fork,su=nobody,reuseaddr TCP6:[$TARGET]:443"
-    command3="sudo socat TCP4-LISTEN:25565,fork,su=nobody,reuseaddr TCP6:[$TARGET]:25565"
-    command4="sudo socat UDP4-LISTEN:24454,fork,su=nobody,reuseaddr UDP6:[$TARGET]:24454"
+
+	if [ -z "$TARGET" ]; then
+    	echo "Target was empty! Full DIG result:"
+		dig AAAA "$(cat target.txt)"
+		exit 1;
+	fi
+
+    command1="run_proxy_tcp \"$TARGET\" 80"
+    command2="run_proxy_tcp \"$TARGET\" 443"
+    command3="run_proxy_tcp \"$TARGET\" 25565"
+    command4="run_proxy_udp \"$TARGET\" 8024454"
     
     # Run commands in the background
     $command1 &
@@ -23,19 +30,27 @@ start_processes() {
     echo "Processes started with PIDs: $pid1, $pid2, $pid3 and $pid4, and targeting $TARGET. Use kill -9 $pid1 $pid2 $pid3 $pid4 to stop."
 }
 
-stop_processes() {
-    echo "Stopping proxies..."
-    kill -9 $pid1 $pid2 $pid3 $pid4 2>/dev/null
+run_proxy_tpc() {
+	while true
+	do
+		sudo socat TCP4-LISTEN:$2,fork,su=nobody,reuseaddr "TCP6:[$1]:$2"
+		echo "Proxy died! Restarting in 5s..."
+		sleep 5;
+	done
+}
+
+run_proxy_upd() {
+	while true
+	do
+		sudo socat UDP4-LISTEN:$2,fork,su=nobody,reuseaddr "UDP6:[$1]:$2"
+		echo "Proxy died! Restarting in 5s..."
+		sleep 5;
+	done
 }
 
 if [ ! -f "target.txt" ]; then
     echo "No target.txt found! Create one with the target domain."
+	exit 1;
 fi
 
 start_processes
-while true; do
-    sleep 3600 # Restart every hour
-
-    stop_processes
-    start_processes
-done
